a5e692eeef60d4ef2fb1a1ee45bfaf6b797bf8c5
Luca Bello <luca.bello96@hotmail.it>
2020-02-12 11:43:37 +0100

    utilities to generate data and compare results using pomegranate

--- python/generate_data.py
+++ python/generate_data.py
@@ -3,23 +3,38 @@ import numpy as np
 
 # Example: create model with two states
 model = pm.HiddenMarkovModel("Two-states HMM")
-s1 = pm.State(pm.NormalDistribution(0, 1), name="State 1")
-s2 = pm.State(pm.NormalDistribution(10, 1), name="State 2")
+s1 = pm.State(pm.NormalDistribution(0, 1), name="0")
+s2 = pm.State(pm.NormalDistribution(10, 1), name="1")
 model.add_states([s1, s2])
 model.add_transition(model.start, s1, 1.0)
-model.add_transition(s1, s1, 0.99)
-model.add_transition(s2, s1, 0.01)
-model.add_transition(s1, s2, 0.01)
-model.add_transition(s2, s2, 0.99)
+model.add_transition(s1, s1, 0.5)
+model.add_transition(s2, s1, 0.5)
+model.add_transition(s1, s2, 0.5)
+model.add_transition(s2, s2, 0.5)
 model.bake()
 
 # generate samples from the model
-chosen_len = 100000
+chosen_len = 10000000
 samples = model.sample(length=chosen_len, path=True, random_state=None)
 observations = samples[0]
 state_path = samples[1]
 
 # print(observations)
-out_file = open("data",'w')
+out_file = open("data", "w")
+obs_written = 1
 for o in observations:
-    out_file.write(str(o) + " ")
+    if obs_written == len(observations):
+        out_file.write(str(o))
+    else:
+        out_file.write(str(o) + " ")
+    obs_written = obs_written + 1
+out_file.close()
+
+# for s in state_path:
+#     print(s.name)
+path_file = open("path", "w");
+for s in state_path:
+    if s.name == ("Two-states HMM-start"):
+        continue
+    path_file.write(str(s.name) + " ")
+path_file.close()

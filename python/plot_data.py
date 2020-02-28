821c343dca97ab84043af7fc9c1c7a96bfd90dfd
Luca Bello <luca.bello96@hotmail.it>
2020-02-28 19:49:29 +0100

    now also print the state in red over the points
    also using the new directory for observations and generating path

--- python/plot_data.py
+++ python/plot_data.py
@@ -1,13 +1,31 @@
 import matplotlib.pyplot as plt
 
-in_file = open("data", "r")
+in_file = open("data/observations", "r")
 line = in_file.read()
 string_list = line.split()
 value_list = []
+input_limit = 10000
+counter = 1
 for s in string_list:
     value_list.append(float(s))
+    counter = counter + 1
+    if counter > input_limit:
+        break
 in_file.close()
 x = range(1,10001)
 
+path_file = open("data/path", "r")
+line = path_file.read()
+string_list = line.split()
+path_list = []
+counter = 1
+for s in string_list:
+    path_list.append(int(s)*10)
+    counter = counter + 1
+    if counter > input_limit:
+        break
+path_file.close()
+
 plt.scatter(x, value_list, color='blue')
+plt.step(x, path_list, color='red')
 plt.show()

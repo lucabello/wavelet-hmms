538a9b17cdd2e7798a2c17bf3dcf0d11258aa337
Luca Bello <luca.bello96@hotmail.it>
2020-03-24 17:12:41 +0100

    added functions to return numerical values

--- python/viterbi_comparison.py
+++ python/viterbi_comparison.py
@@ -2,13 +2,30 @@
 # -*- coding: utf-8 -*-
 import utilities_io as uio
 
+# OPTIONS
+verbose = False
+
 def count_differences(p1, p2):
     difference_count = 0
     for i in range(0, len(p1)):
         if p1[i] != p2[i]:
             difference_count = difference_count + 1
-    print("Number of matches: ", len(p1)-difference_count)
-    print("Number of differences: ", difference_count)
+            if verbose:
+                print("index:",i,"p1:",p1[i],"p2:",p2[i])
+    if verbose:
+        print("Number of matches: ", len(p1)-difference_count)
+        print("Number of differences: ", difference_count)
+    return difference_count
+
+def count_differences_uncompressed():
+    state_path = uio.read_path()
+    wahmm_path = uio.read_path("results/decoding_path")
+    return count_differences(state_path, wahmm_path)
+
+def count_differences_compressed():
+    state_path = uio.read_path()
+    compressed_path = uio.read_path("results/compressed_decoding_path")
+    return count_differences(state_path, compressed_path)
 
 pm_path = uio.read_path("results/pm_decoding_path")
 wahmm_path = uio.read_path("results/decoding_path")

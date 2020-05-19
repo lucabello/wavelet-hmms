382047c777c582bcec1a958b8588507b2fe2dde1
Luca Bello <bluca@bayes.ita.chalmers.se>
2020-05-19 10:35:31 +0200

    using provided parameter for number of processes

--- python/parallel_testing.py
+++ python/parallel_testing.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 import multiprocessing
 import subprocess
+import os
 
 # Paremeters arrays
 topologies = ["FC", "CI", "LR"]
-states = [2, 3, 5, 10, 50, 100]
+states = [2, 3, 5, 10, 20, 50, 100]
 etas = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
 
 # Function to deploy tests
@@ -23,11 +24,11 @@ def test(params):
 
 # configuration
 t = topologies[0]
-s = states[0]
+s = 10
 configuration = []
 for eta in etas:
     configuration.append([t, s, eta])
 
 # start tests
-p = multiprocessing.Pool(10)
+p = multiprocessing.Pool(len(os.sched_getaffinity(0)))
 s = p.map(test, configuration)
